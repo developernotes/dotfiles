210df4556e67c9170cff700fcd541a8b63af87fb
Nick Parker <nparker@geolearning.com>
2011-04-16 16:43:07 -0500

    Adding additional entries to the PATH

--- custom/export.zsh
+++ custom/export.zsh
@@ -1,11 +1,7 @@
 
 export CDPATH=~/code
 
-export PATH=~/bin/darcs:$PATH
 export PATH=~/bin/scripts:$PATH
-
-# git
-export GIT_EDITOR="$ZSH/custom/eopen.sh"
 
 case `uname` in
     Darwin)
@@ -15,13 +11,25 @@ case `uname` in
         ;;
     CYGWIN_NT-6.0)
         export JAVA_HOME=/cygdrive/c/Program\ Files/Java/jdk1.6.0_24
+        export PATH=/cygdrive/c/Program\ Files/MySQL/MySQL\ Server\ 5.1/bin:$PATH
+        export PATH=/cygdrive/c/Eclipse/plugins/net.rim.ejde/vmTools:$PATH
         ;;
 esac
 
+export PATH=~/bin/darcs:$PATH
+
+# git
+export GIT_EDITOR="$ZSH/custom/eopen.sh"
+
 # java
 export M2_HOME=~/bin/maven
+export PATH=~/bin/ant/bin:$PATH
 export PATH=${M2_HOME}/bin:$PATH
 export PATH=${JAVA_HOME}/bin:$PATH
+
+# scala
+export PATH=~/bin/scala/bin:$PATH
+export PATH=~/bin/sbt:$PATH
 
 # clojure
 export PATH=~/bin/leiningen/bin:$PATH
