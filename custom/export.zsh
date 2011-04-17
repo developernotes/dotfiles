8e793ca16f6ae336b48134728147e37e75b378c5
Nick Parker <nparker@geolearning.com>
2011-04-17 10:53:22 -0500

    setting the GIT_EDITOR for osx, moving the scala path for cygwin

--- custom/export.zsh
+++ custom/export.zsh
@@ -6,14 +6,17 @@ export PATH=~/bin/scripts:$PATH
 case `uname` in
     Darwin)
         export JAVA_HOME=$(/usr/libexec/java_home)
+        export GIT_EDITOR="$ZSH/custom/eopen.sh"
         [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
         ;;
     CYGWIN*)
         export CYGWIN="nodosfilewarning"
+        export PATH=~/bin/sbt:$PATH
+        export PATH=~/bin/scala/bin:$PATH
+        export GIT_EDITOR="`cygpath -m $ZSH`//custom//eopen.sh"
         export JAVA_HOME=/cygdrive/c/Program\ Files/Java/jdk1.6.0_24
         export PATH=/cygdrive/c/Program\ Files/MySQL/MySQL\ Server\ 5.1/bin:$PATH
         export PATH=/cygdrive/c/Eclipse/plugins/net.rim.ejde/vmTools:$PATH
-        export GIT_EDITOR="`cygpath -m $ZSH`//custom//eopen.sh"
         ;;
 esac
 
@@ -24,10 +27,6 @@ export M2_HOME=~/bin/maven
 export PATH=~/bin/ant/bin:$PATH
 export PATH=${M2_HOME}/bin:$PATH
 export PATH=${JAVA_HOME}/bin:$PATH
-
-# scala
-export PATH=~/bin/scala/bin:$PATH
-export PATH=~/bin/sbt:$PATH
 
 # clojure
 export PATH=~/bin/leiningen/bin:$PATH
