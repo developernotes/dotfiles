dfec9479ba1c97af8e6fcee2177712e9fe471f0d
Nick Parker <nparker@geolearning.com>
2011-04-17 10:40:42 -0500

    aliases and git editor are set allow different versions of cygwin

--- custom/export.zsh
+++ custom/export.zsh
@@ -6,20 +6,18 @@ export PATH=~/bin/scripts:$PATH
 case `uname` in
     Darwin)
         export JAVA_HOME=$(/usr/libexec/java_home)
-        # rvm
         [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
         ;;
-    CYGWIN_NT-6.0)
+    CYGWIN*)
+        export CYGWIN="nodosfilewarning"
         export JAVA_HOME=/cygdrive/c/Program\ Files/Java/jdk1.6.0_24
         export PATH=/cygdrive/c/Program\ Files/MySQL/MySQL\ Server\ 5.1/bin:$PATH
         export PATH=/cygdrive/c/Eclipse/plugins/net.rim.ejde/vmTools:$PATH
+        export GIT_EDITOR="`cygpath -m $ZSH`//custom//eopen.sh"
         ;;
 esac
 
 export PATH=~/bin/darcs:$PATH
-
-# git
-export GIT_EDITOR="$ZSH/custom/eopen.sh"
 
 # java
 export M2_HOME=~/bin/maven
